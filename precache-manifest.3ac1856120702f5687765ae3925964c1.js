92d28ca389691bd82d478b196e828b73454b6602
Michael Silver <rush340@gmail.com>
2020-06-03 12:10:30 -0700

    Updates

---
+++ precache-manifest.3ac1856120702f5687765ae3925964c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d13c1ba348898b3113cb39e11646fad",
+    "url": "/dnd-ability-calc/index.html"
+  },
+  {
+    "revision": "2fa84b8a9eab77beb429",
+    "url": "/dnd-ability-calc/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "7118a49b9457976c3bb4",
+    "url": "/dnd-ability-calc/static/js/2.2bb54c66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dnd-ability-calc/static/js/2.2bb54c66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fa84b8a9eab77beb429",
+    "url": "/dnd-ability-calc/static/js/main.e4a67d9a.chunk.js"
+  },
+  {
+    "revision": "7b91d6682a5deec8a508",
+    "url": "/dnd-ability-calc/static/js/runtime-main.c4e589c8.js"
+  }
+]);
