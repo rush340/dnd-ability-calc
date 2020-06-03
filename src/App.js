2819c2ac476b23cc6b00fab010f73111ba0fe8f7
Michael Silver <rush340@gmail.com>
2020-06-03 15:54:52 -0700

    slight improvements to mobile styling

--- src/App.js
+++ src/App.js
@@ -185,7 +185,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="app">
         <h1>D&amp;D 5e Point Buy Calculator</h1>
         <Points
           remaining={this.state.points}
