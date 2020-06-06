ef47b6067240614085d29de8708b3a243e1d422c
Michael Silver <rush340@gmail.com>
2020-06-05 17:56:02 -0700

    moved ability components to module

---
+++ src/AbilitiesPanel.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import {
+  SCORE_COSTS,
+  getAbilityModifierString
+} from './abilities.js';
+import './AbilitiesPanel.css';
+
+
+function AbilityChangeControl(props) {
+  const label = props.increase ? 'Increase' : 'Decrease';
+
+  // we show a minus for positive costs because we're conveying the effect
+  // on the remaining point value
+  const plusOrMinus = props.cost >= 0 ? '-' : '+';
+  const costLabel = props.disabled ?
+    '' :
+    `(${plusOrMinus}${Math.abs(props.cost)} point${Math.abs(props.cost) > 1 ? 's' : ''})`;
+  return (
+    <div className="ability-change-control">
+      <button
+        onClick={props.onClick}
+        disabled={props.disabled}
+      >
+        {label}<br />
+        {costLabel}
+      </button>
+    </div>
+  );
+}
+
+function AbilityDisplay(props) {
+  const modifier = getAbilityModifierString(props.score);
+  return (
+    <div className="ability">
+      <div className="ability-name"><strong>{props.name}</strong></div>
+      <div>Score: {props.score}</div>
+      <div>Modifier: {modifier}</div>
+      <AbilityChangeControl
+        increase={true}
+        onClick={props.increaseClick}
+        cost={props.increaseCost}
+        disabled={!props.canIncrease}
+      />
+      <AbilityChangeControl
+        increase={false}
+        onClick={props.decreaseClick}
+        cost={props.decreaseCost}
+        disabled={!props.canDecrease}
+      />
+    </div>
+  );
+}
+
+function AbilitiesPanel(props) {
+  const abilities = [];
+  for (let [ability, score] of Object.entries(props.abilityScores)) {
+    const increaseClick = () => props.increaseAbility(ability);
+    const decreaseClick = () => props.decreaseAbility(ability);
+
+    const increaseCost = SCORE_COSTS[score + 1] === undefined ?
+      null :
+      SCORE_COSTS[score + 1] - SCORE_COSTS[score];
+    const decreaseCost = SCORE_COSTS[score - 1] === undefined ?
+      null :
+      SCORE_COSTS[score - 1] - SCORE_COSTS[score];
+    abilities.push(<AbilityDisplay
+      key={ability}
+      name={ability}
+      score={score}
+      increaseClick={increaseClick}
+      decreaseClick={decreaseClick}
+      increaseCost={increaseCost}
+      decreaseCost={decreaseCost}
+      canIncrease={increaseCost !== null && increaseCost <= props.points}
+      canDecrease={decreaseCost !== null}
+    />);
+  }
+
+  return (
+    <div className="abilities-container">
+      {abilities}
+    </div>
+  );
+}
+
+export default AbilitiesPanel;
--- src/App.js
+++ src/App.js
@@ -1,94 +1,14 @@
-// TODO refactor
-
 import React from 'react';
 import {
   SCORE_COSTS,
   DEFAULT_INITIAL_POINTS,
   INITIAL_ABILITY_SCORES,
-  getAbilityModifierString
 } from './abilities.js';
+import AbilitiesPanel from './AbilitiesPanel.js';
 import './App.css';
 
 
-function AbilityChangeControl(props) {
-  const label = props.increase ? 'Increase' : 'Decrease';
-
-  // we show a minus for positive costs because we're conveying the effect
-  // on the remaining point value
-  const plusOrMinus = props.cost >= 0 ? '-' : '+';
-  const costLabel = props.disabled ?
-    '' :
-    `(${plusOrMinus}${Math.abs(props.cost)} point${Math.abs(props.cost) > 1 ? 's' : ''})`;
-  return (
-    <div className="ability-change-control">
-      <button
-        onClick={props.onClick}
-        disabled={props.disabled}
-      >
-        {label}<br />
-        {costLabel}
-      </button>
-    </div>
-  );
-}
-
-function Ability(props) {
-  const modifier = getAbilityModifierString(props.score);
-  return (
-    <div className="ability">
-      <div className="ability-name"><strong>{props.name}</strong></div>
-      <div>Score: {props.score}</div>
-      <div>Modifier: {modifier}</div>
-      <AbilityChangeControl
-        increase={true}
-        onClick={props.increaseClick}
-        cost={props.increaseCost}
-        disabled={!props.canIncrease}
-      />
-      <AbilityChangeControl
-        increase={false}
-        onClick={props.decreaseClick}
-        cost={props.decreaseCost}
-        disabled={!props.canDecrease}
-      />
-    </div>
-  );
-}
-
-function Abilities(props) {
-  const abilities = [];
-  for (let [ability, score] of Object.entries(props.abilityScores)) {
-    const increaseClick = () => props.increaseAbility(ability);
-    const decreaseClick = () => props.decreaseAbility(ability);
-
-    const increaseCost = SCORE_COSTS[score + 1] === undefined ?
-      null :
-      SCORE_COSTS[score + 1] - SCORE_COSTS[score];
-    const decreaseCost = SCORE_COSTS[score - 1] === undefined ?
-      null :
-      SCORE_COSTS[score - 1] - SCORE_COSTS[score];
-    abilities.push(<Ability
-      key={ability}
-      name={ability}
-      score={score}
-      increaseClick={increaseClick}
-      decreaseClick={decreaseClick}
-      increaseCost={increaseCost}
-      decreaseCost={decreaseCost}
-      canIncrease={increaseCost !== null && increaseCost <= props.points}
-      canDecrease={decreaseCost !== null}
-    />);
-  }
-
-  return (
-    <div className="abilities-container">
-      {abilities}
-    </div>
-  );
-
-}
-
-function Points(props) {
+function RemainingPointsDisplay(props) {
   return (
     <div className="points-container">
       <div><strong>Remaining points:</strong></div>
@@ -96,7 +16,6 @@ function Points(props) {
     </div>
   );
 }
-
 
 class App extends React.Component {
   constructor(props) {
@@ -147,11 +66,11 @@ class App extends React.Component {
     return (
       <div className="app">
         <h1>D&amp;D 5e Point Buy Calculator</h1>
-        <Points
+        <RemainingPointsDisplay
           remaining={this.state.points}
           max={DEFAULT_INITIAL_POINTS}
         />
-        <Abilities
+        <AbilitiesPanel
           abilityScores={this.state.abilities}
           increaseAbility={this.increaseAbility}
           decreaseAbility={this.decreaseAbility}
